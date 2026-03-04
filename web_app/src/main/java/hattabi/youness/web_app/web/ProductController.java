a942feab5b0f9ae4ce5e54b36ecb056029941d89
HATTABIYounessN7 <youness.hattabi-etu@etu.univh2c.ma>
2026-03-04 12:26:42 +0000

    Add delete functionality to product listing page

--- web_app/src/main/java/hattabi/youness/web_app/web/ProductController.java
+++ web_app/src/main/java/hattabi/youness/web_app/web/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import hattabi.youness.web_app.repositories.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,6 +17,12 @@ public class ProductController {
     public String products(Model model) {
         model.addAttribute("products", productRepository.findAll());
         return "products";
+    }
+
+    @GetMapping("/deleteProduct")
+    public String deleteProduct(@RequestParam Long id) {
+        productRepository.deleteById(id);
+        return "redirect:/products";
     }
 
 }
